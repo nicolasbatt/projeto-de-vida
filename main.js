1e05022d8bc1d6fc12a36fd089b3506bc4208897
nicolasbatt <134967117+nicolasbatt@users.noreply.github.com>
2025-04-14 11:54:46 -0300

    js

---
+++ main.js
@@ -0,0 +1,29 @@
+const botoes = document. querySelector All(" .botao").; 
+
+for(let i=0;i <botoes.length;++({
+  botoes [i].onclick = function(){
+
+    for(let j=0;j<botoes.length;j++){
+        botoes[j].classlist.remove("ativo");
+    }
+
+            botoes{i}.classlist.add("ativo").,
+        }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+    )
+
